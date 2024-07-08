cc3d472f1aa1f3b38991172f28aacccb1fa7bf82
unknown <t.m.ahanafshahriar@gmail.com>
2024-07-08 14:59:26 +0600

    JS added and CSS modified. Project Complete

---
+++ script.js
@@ -0,0 +1,42 @@
+const counterEl = document.querySelector(".counter");
+const counterTitleEl = document.querySelector(".counter__title");
+const counterValEl = document.querySelector(".counter__value");
+
+const plusBtn = document.querySelector(".counter__btn-plus");
+const minusBtn = document.querySelector(".counter__btn-minus");
+const resetBtn = document.querySelector(".counter__reset-btn");
+
+function incrementVal() {
+  const currentVal = parseInt(counterValEl.textContent);
+  let newVal = currentVal + 1;
+  if (newVal > 5) {
+    newVal = 5;
+    counterEl.classList.add("counter-limit");
+    counterTitleEl.textContent = "Limit Reached! Buy PRO Version for > 5";
+    plusBtn.disabled = true;
+    minusBtn.disabled = true;
+  }
+  counterValEl.textContent = newVal;
+  plusBtn.blur();
+}
+
+plusBtn.addEventListener("click", incrementVal);
+
+minusBtn.addEventListener("click", function () {
+  const currentVal = parseInt(counterValEl.textContent);
+  let newVal = currentVal - 1;
+  if (newVal < 0) newVal = 0;
+  counterValEl.textContent = newVal;
+  minusBtn.blur();
+});
+
+resetBtn.addEventListener("click", function () {
+  counterValEl.textContent = 0;
+  counterEl.classList.remove("counter-limit");
+  counterTitleEl.textContent = "cool counter";
+  plusBtn.disabled = false;
+  minusBtn.disabled = false;
+  plusBtn.blur();
+  minusBtn.blur();
+});
+document.addEventListener("keydown", incrementVal);
